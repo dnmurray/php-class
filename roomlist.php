c2662a21e906d110616b8273c9eb8220d444c994
alex <abl12582@gmail.com>
2015-04-18 14:22:47 -0400

    New roomlist.php file

---
+++ roomlist.php
@@ -0,0 +1,19 @@
+<!doctype HTML>
+
+<html lang="en-US">
+
+<head>
+	<title>Room List</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Room list for hotel">
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+
+<body>
+	<div class="main-wrap">
+	<a href="room.php"></a>
+	</div>	
+</body>
+
+</html>
